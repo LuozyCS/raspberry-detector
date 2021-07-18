a0725a7a211e6fa292117eb24a0121b428ae60e4
lzy_cs <Luozy_CS@163.com>
2021-07-18 14:56:30 +0800

    init

---
+++ src/main/java/com/my/raspberrydetecor/RaspberryDetecorApplication.java
@@ -0,0 +1,13 @@
+package com.my.raspberrydetecor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RaspberryDetecorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RaspberryDetecorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/my/raspberrydetecor/RaspberryDetecorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.my.raspberrydetecor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RaspberryDetecorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
