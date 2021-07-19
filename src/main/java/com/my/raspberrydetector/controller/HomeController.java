5d5ff26bbdbd7b9eeba8cac1e1a1642f3c504767
lzy_cs <Luozy_CS@163.com>
2021-07-19 23:08:58 +0800

    前端部分

--- src/main/java/com/my/raspberrydetector/controller/HomeController.java
+++ src/main/java/com/my/raspberrydetector/controller/HomeController.java
@@ -6,6 +6,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import javax.servlet.http.HttpServletRequest;
+
 @Controller
 public class HomeController {
     @Autowired
@@ -16,7 +18,7 @@ public class HomeController {
             Model model
     ){
         System.out.println(dht11Mapper.getAllInfo());
-        model.addAttribute("DHT",dht11Mapper.getAllInfo());
+        model.addAttribute("DHT11",dht11Mapper.getAllInfo());
 //        model.addAttribute("");
         return "home";
     }
