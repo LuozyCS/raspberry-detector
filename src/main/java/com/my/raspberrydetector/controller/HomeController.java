80083328170eaa14b3a0ba560e72651f1ee4a3bd
lzy_cs <Luozy_CS@163.com>
2021-07-20 11:31:12 +0800

    nearly success

--- src/main/java/com/my/raspberrydetector/controller/HomeController.java
+++ src/main/java/com/my/raspberrydetector/controller/HomeController.java
@@ -1,12 +1,19 @@
 package com.my.raspberrydetector.controller;
 
+import com.my.raspberrydetector.dataAccessObject.DHT11;
 import com.my.raspberrydetector.dataAccessObject.DHT11Mapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.servlet.http.HttpServletRequest;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 @Controller
 public class HomeController {
@@ -17,9 +24,30 @@ public class HomeController {
     public  String home(
             Model model
     ){
-        System.out.println(dht11Mapper.getAllInfo());
-        model.addAttribute("DHT11",dht11Mapper.getAllInfo());
-//        model.addAttribute("");
+//        System.out.println(dht11Mapper.getAllInfo());
+
         return "home";
+    }
+
+    @ResponseBody
+    @RequestMapping("/home_request")
+    public List<DHT11> homeRequest(
+
+    ){
+        List<DHT11> DHT11List_rec =  dht11Mapper.getAllInfo().subList(0,Math.min(15, dht11Mapper.getAllInfo().size()));
+        for(DHT11 each:DHT11List_rec){
+            each.setTIME(each.getTIME().substring(each.getTIME().indexOf(" "),each.getTIME().indexOf(" ")+6));
+        }
+        Collections.reverse(DHT11List_rec);
+        return DHT11List_rec;
+    }
+
+    @ResponseBody
+    @RequestMapping("/home_request_table")
+    public ArrayList<DHT11> homeRequestTable(
+    ){
+        ArrayList<DHT11> DHT11List = (ArrayList<DHT11>) dht11Mapper.getAllInfo();
+        return DHT11List;
+
     }
 }
